ca8738e8276f281d10ea7a98411d50bec78ba315
leebo <me@bobo.im>
2015-11-19 13:36:13 +0800

    构架目录接口加入webpack

---
+++ src/app.vue
@@ -0,0 +1,26 @@
+<template lang="jade">
+div
+  h1 {{msg}}
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello from vue-loader!'
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style lang="stylus">
+primary-color = #999
+body
+  font 100% font-stack
+  color primary-color
+.logo
+  width 40px
+  height 40px
+</style>
