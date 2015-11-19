f999126b307f4e351e672fc820098d00209dedf3
leebo <me@bobo.im>
2015-11-19 14:04:01 +0800

    组件化菜单

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 var Vue = require('vue')
 var App = require('./app.vue')
+var Menu = require('./components/menu.vue')
 var Zepto = require('../node_modules/zepto/zepto.min.js')
 require('./assets/js/sm.js')
 
 new Vue({
   el: 'body',
   components: {
-    app: App
+    app: App,
+    menu: Menu
   }
 })
