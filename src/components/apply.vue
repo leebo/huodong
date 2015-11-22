4ff9e902069e23797b5a4b12ddd674f434e825f4
leebo <me@bobo.im>
2015-11-22 14:20:18 +0800

    参赛说明

--- src/components/apply.vue
+++ src/components/apply.vue
@@ -5,9 +5,9 @@
       <li>
         <div class="item-content">
           <div class="item-inner">
-            <div class="item-title label">姓名</div>
+            <div class="item-title label">宝贝昵称</div>
             <div class="item-input">
-              <input type="text" placeholder="请填写萌娃姓名">
+              <input type="text" v-model="user.name" placeholder="请填写萌娃姓名">
             </div>
           </div>
         </div>
@@ -15,9 +15,9 @@
       <li>
         <div class="item-content">
           <div class="item-inner">
-            <div class="item-title label">年龄</div>
+            <div class="item-title label">宝贝年龄</div>
             <div class="item-input">
-              <input type="number" placeholder="请填写萌娃年龄">
+              <input type="number" v-model="user.age" placeholder="请填写萌娃年龄">
             </div>
           </div>
         </div>
@@ -27,7 +27,7 @@
           <div class="item-inner">
             <div class="item-title label">参赛宣言</div>
             <div class="item-input">
-              <textarea placeholder="请填写参赛宣言，五个字以上"></textarea>
+              <textarea v-model="user.title" placeholder="请填写参赛宣言，五个字以上"></textarea>
             </div>
           </div>
         </div>
@@ -36,8 +36,8 @@
   </div>
   <div class="content-block margin-top-0">
     <div class="row">
-      <div class="col-50"><a href="#" class="button button-sm button-fill button-danger">取消</a></div>
-      <div class="col-50"><a href="#" class="button button-sm button-fill button-success">提交</a></div>
+      <div class="col-50"><a href="javascript:void(0)" class="button button-sm button-fill button-danger" v-on:click="cancel">重 填</a></div>
+      <div class="col-50"><a href="javascript:void(0)" class="button button-sm button-fill button-success" v-on:click="save">提 交</a></div>
     </div>
   </div>
 </div>
@@ -48,12 +48,32 @@ export default {
   props: ['title'],
   data () {
     return {
+      user: {},
     }
   },
+
   components: {
   },
+
+  created: function(){
+    $.showIndicator()
+  },
+
   ready: function(){
-    this.$data.title = "活动报名"
-  }
+    this.title = "活动报名"
+    $.hideIndicator()
+  },
+
+  methods: {
+    save: function(){
+      $.toast("活动报名成功");
+    },
+    cancel: function(){
+      var that = this;
+      $.confirm('是否确认要取消报名', function(){
+        that.user = {}
+      });
+    },
+  },
 }
 </script>
--- src/components/tips.vue
+++ src/components/tips.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="content margin-bottom-lg">{{user}}</div>
+<div class="content margin-bottom-lg">
+  <div class="card">
+    <div class="card-header">活动奖品说明</div>
+    <div class="card-content">
+      <div class="card-content-inner">头和尾的卡片。卡头是用来显示一些额外的信息，或自定义操作卡标题和页脚。</div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header">活动参赛要求</div>
+    <div class="card-content">
+      <div class="card-content-inner">头和尾的卡片。卡头是用来显示一些额外的信息，或自定义操作卡标题和页脚。</div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header">活动投票须知</div>
+    <div class="card-content">
+      <div class="card-content-inner">头和尾的卡片。卡头是用来显示一些额外的信息，或自定义操作卡标题和页脚。</div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
